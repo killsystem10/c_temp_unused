268374ae0d0413074c88624644366f9743d05ced
killsystem10 <killsystem10@outlook.com>
2024-09-03 10:08:42 +0900

    스택 큐 연습

--- scanftest.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int dasdf() {
-    int a;
-    printf("input int:");
-    scanf("%d", &a);
-    printf("%d", a);
-    return 0;
-}
--- test.cpp
+++ test.cpp
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <stack>
+std::stack<int> S;
+
 int main() {
-    printf("test");
+	for (int i = 1; i <= 3; i++) {
+		S.push(i);
+	}
+	printf("%d ", S.top());
+	S.pop();
+
+	S.push(4);
+	S.push(5);
+	while (!S.empty()) {
+		printf("%d ", S.top());
+		S.pop();
+	}
 }
